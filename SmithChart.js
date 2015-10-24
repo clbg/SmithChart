0bf4b84544df532651f6e58546e3c91598956882
TangZonxun <stardust@live.hk>
2015-10-24 22:35:10 +0800

    Part I finished!

--- SmithChart.js
+++ SmithChart.js
@@ -18,8 +18,8 @@ SmithChart.prototype.drawZIm=function(ZIm, color){
 	var theta;
 	theta=Math.atan(ZIm)
 	this.ctx.beginPath();
-	
-	
+
+
 	var color = arguments[1] ? arguments[1] : "#000080";
 	this.ctx.strokeStyle = color
 	if(ZIm===0){
@@ -32,7 +32,7 @@ SmithChart.prototype.drawZIm=function(ZIm, color){
 	else{
 		this.ctx.arc(nz(2),nz(1-1/ZIm),Math.abs(nz(1/ZIm)),Math.PI*1.5,Math.PI*1.5+2*theta,true)
 	}
-	
+
 	this.ctx.stroke();
 }
 
@@ -58,10 +58,10 @@ SmithChart.prototype.drawCoord=function(color){
 	this.ctx.lineTo(nz(1),nz(2));
 	this.ctx.stroke();
 }
-	
+
 // 清空背景，然后重新绘制整个圆图（包括两种圆，横纵轴）
 SmithChart.prototype.drawChart=function(color){
-	
+
 	this.ctx.clearRect (0,0,nz(2),nz(2));
 	this.ctx.beginPath();
 	var color = arguments[1] ? arguments[1] : "#000000";
@@ -73,7 +73,7 @@ SmithChart.prototype.drawChart=function(color){
 	this.ctx.moveTo(nz(1),nz(0));
 	this.ctx.lineTo(nz(1),nz(2));
 	var list1=[];
-	
+
 	//draw losts of ZRe
 	for(var R=0.1;R<1;R=R+0.1)
 		list1.push(R)
@@ -83,10 +83,10 @@ SmithChart.prototype.drawChart=function(color){
 		list1.push(R);
 	list1.push(5,10,20,50);
 	list1.map(this.drawZRe);
-	
+
 	var list2=[];
 	//draw losts of ZIm
-	
+
 	for(var R =0;R <1; R=R+0.1)
 		list2.push(R);
 	for(R=1;R<2;R=R+0.2)
@@ -94,21 +94,21 @@ SmithChart.prototype.drawChart=function(color){
 	for(R=2;R<5;R=R+1)
 		list2.push(R);
 	list2.push(5,10,20,50);
-	
-	
+
+
 		function inve(x){
 			return -x;
 		}
-		
+
 	list22=list2.map(inve);
 	list2=list2.concat(list22);
 	list2.map(this.drawZIm);
 
-	
-	
-	
-	
-	
+
+
+
+
+
 	this.ctx.stroke();
 }
 
@@ -116,39 +116,10 @@ SmithChart.prototype.drawChart=function(color){
 
 // 画同心扇形（算输入阻抗和阻抗匹配时可能需要用）
 SmithChart.prototype.drawArc=function(start, angle, color){
-	
-} 
+
+}
 
 
 
 
-
-/*
-var canvas=document.getElementById('cv');
-canvas=SmithChart(canvas);
-var ctx=canvas.getContext('2d');
-ctx.fillStyle='#FF0000';
-ctx.fillRect(0,0,600,600);
-
-*/
-
-
-//var canvas = document.getElementById("cv");
-//var ctx = canvas.getContext("2d");
-//var SC=new SmithChart(ctx);
-
-
-		
-	//SC.ctx.strokeRect(0,0,600,600);
-	
-	//SC.drawChart();
-
-	
-	alert("同学您好！130222班 杨力同学诚招女友，微信：“yangliTXWD”，非诚勿扰~");
-	
-	
-	
-	
-
-
-
+alert("同学您好！130222班 杨力同学诚招女友，微信：“yangliTXWD”，非诚勿扰~");
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 var canvas = document.getElementById("cv");
 var ctx = canvas.getContext("2d");
 var smithChart = new SmithChart(ctx);
@@ -17,8 +16,8 @@ function updateValueAndChart(impedence, gamma){
   setValue(impedence.r, "ZL_re");
   setValue(impedence.i, "ZL_im");
   smithChart.drawChart();
-  smithChart.drawZIm(impedence.i);
-  smithChart.drawZRe(impedence.r);
+  smithChart.drawZIm(impedence.i, "#FF0000");
+  smithChart.drawZRe(impedence.r, "#FF0000");
 
   setValue(gamma.r, "gamma_re");
   setValue(gamma.i, "gamma_im");
@@ -52,3 +51,52 @@ function ZRiOK(){
   }
   fn(getValue("ZL_re"), getValue("ZL_im"));
 }
+
+var cvFixed = 0;
+
+function getX(obj){
+   var tmp = obj.offsetLeft;
+   var val = obj.offsetParent;
+   while(val != null){
+       tmp += val.offsetLeft;
+       val = val.offsetParent;
+   }
+   return tmp;
+}
+
+function getY(obj){
+   var tmp = obj.offsetTop;
+   var val = obj.offsetParent;
+   while(val != null){
+     tmp += val.offsetTop;
+     val = val.offsetParent;
+   }
+   return tmp;
+}
+
+function cvMove(e){
+  if(cvFixed == 0){
+    var x = e.clientX;
+    var y = e.clientY;
+    var cv = document.getElementById("cv");
+    x = x - getX(cv) + document.body.scrollLeft;
+    y = y - getY(cv) + document.body.scrollTop;
+    console.log(x,y);
+    x = x / 300.0 - 1.0;
+    y = 1.0 - y / 300.0;
+    var gamma = new Complex(x, y);
+    var impedence = new Complex(1,0).add(gamma).mul(new Complex(1,0).add(gamma.neg()).inv());
+    updateValueAndChart(impedence, gamma);
+  }
+}
+
+function cvClick(){
+  if(cvFixed == 0){
+    cvFixed = 1;
+  }
+  else{
+    cvFixed = 0;
+  }
+}
+
+smithChart.drawChart();
